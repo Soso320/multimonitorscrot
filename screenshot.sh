841afd5d6b077eff6d33387df7af3b0478029b0b
SosoPlayz <43059015+SosoPlayz@users.noreply.github.com>
2020-06-23 17:22:51 +0300

    Create screenshot.sh

---
+++ screenshot.sh
@@ -0,0 +1,8 @@
+/bin/bash
+now=$(date +"Screenshot_%m_%d_%Y__%::z")
+cd ~/Pictures/bruh
+scrot TEMPORARYSHOT.png
+convert -crop +1920+0 TEMPORARYSHOT.png TEMPORARYSHOT2.png
+rm TEMPORARYSHOT.png
+convert -crop -1920+0 TEMPORARYSHOT2.png $now.png
+rm TEMPORARYSHOT2.png
